4d5e3148dc78d3c3690332e34794e0f75ab17d49
evhenii--syrovatka <evsyrovatka@gmail.com>
2024-02-22 16:39:11 +0200

    add sign up page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
-import { FC } from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
 
 const Home: FC = () => {
+  const router = useRouter();
+
+  // TODO: add state manager for user auth
+  const isUserInit = false;
+  const [isUserLoad, setIsUserLoad] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsUserLoad(false);
+  }, []);
+
+  if (isUserLoad) return <div>Loading...</div>;
+
+  if (!isUserInit) router.push("/auth/signup");
+
   return <main>Stadiums</main>;
 };
 
