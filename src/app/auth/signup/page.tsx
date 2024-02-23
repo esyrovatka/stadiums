01623057f21f99acb21aa737a690cfa40d931aaf
evhenii--syrovatka <evsyrovatka@gmail.com>
2024-02-23 10:34:35 +0200

    add webpack config, add global styles, add typography

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react"
 import styles from "./page.module.scss"
+import { Typography } from "@/shared"
 
 const SignUp: FC = () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Sign Up</h1>
+      <Typography tag="h1" weight="six">
+        Sign Up
+      </Typography>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import { FC, PropsWithChildren } from "react";
-import type { Metadata } from "next";
-
+import { FC, PropsWithChildren } from "react"
+import type { Metadata } from "next"
+import "@/shared/styles/main.scss"
 export const metadata: Metadata = {
   title: "Stadiums",
   description: "Stadiums",
-};
+}
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
